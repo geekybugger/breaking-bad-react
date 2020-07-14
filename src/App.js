41bdd67f3c108148dfee38b18fcb52e7401016c3
Krishnakumar V <krishkumarv2001@gmail.com>
2020-07-15 01:12:39 +0530

    Fetched API and presented.. Added styles.. needs to be mobile-friendly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "./components/ui/Header.js";
+import CharacterGrid from "./components/characters/CharacterGrid.js";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <Header />
+      <CharacterGrid isloading={isloading} items={items} />
     </div>
   );
 };
