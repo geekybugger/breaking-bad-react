f763784209326cc2d40dc354ab5bf90a8389ef95
Krishnakumar V <krishkumarv2001@gmail.com>
2020-07-23 17:28:44 +0530

    Updates

---
+++ precache-manifest.53bc9aece151c2a4cbf003b345b09746.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae3775dda50b4f542d2011b81635247",
+    "url": "/breaking-bad-react/index.html"
+  },
+  {
+    "revision": "18d49480763e0f196b76",
+    "url": "/breaking-bad-react/static/css/main.2f676849.chunk.css"
+  },
+  {
+    "revision": "ea36dd62ec2a50d216ff",
+    "url": "/breaking-bad-react/static/js/2.2e8e3f6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-react/static/js/2.2e8e3f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d49480763e0f196b76",
+    "url": "/breaking-bad-react/static/js/main.e9d9062a.chunk.js"
+  },
+  {
+    "revision": "32a6431dc58f01ee00a2",
+    "url": "/breaking-bad-react/static/js/runtime-main.5ab63ded.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-react/static/media/logo.3795f66d.png"
+  }
+]);
